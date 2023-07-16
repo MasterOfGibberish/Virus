457ad947513d760d6ef498614d30cbf752ad96a9
Laimis Silkaitis <92628961+MasterOfGibberish@users.noreply.github.com>
2023-07-16 21:36:51 +0300

    Add files via upload

---
+++ Virus1/BegalinisPaleidimas.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Media;
+
+namespace Virus1
+{
+    /// <summary>
+    /// Interaction logic for BegalinisPaleidimas.xaml
+    /// </summary>
+    public partial class BegalinisPaleidimas : Window
+    {
+        public BegalinisPaleidimas()
+        {
+            InitializeComponent();
+        }
+
+        private void migtukas3_Click(object sender, RoutedEventArgs e)
+        {
+           
+           
+            this.Close();
+        }
+
+        private void Animacija_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+           
+            this.Close();
+        }
+
+        private void Submit_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Passwordas.Password != "4761")
+            {
+             
+                Passwordas.Clear();
+            }
+            else if (Passwordas.Password == "4761")
+            {
+                this.Close();
+            }
+        }
+    }
+}
